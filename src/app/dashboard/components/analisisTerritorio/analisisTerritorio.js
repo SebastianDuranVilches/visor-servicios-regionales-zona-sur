b332367c1caa1c32ae3a95d5dbdbd114e9c93d4d
SDuran <sduran.vilches@gmail.com>
2023-09-11 21:57:22 -0300

    formulario y grafico de radio integrado en diseño

---
+++ src/app/dashboard/components/analisisTerritorio/analisisTerritorio.js
@@ -0,0 +1,63 @@
+import React from "react";
+import "./analisisTerritorio.css";
+import { Row, Col, Container, Form } from "react-bootstrap";
+import RadarChartAnalisis from "./radarChart";
+
+export default class AnalisisTerritorio extends React.Component {
+  render() {
+    return (
+      <div className="analisisTerritorio p-1">
+        <Row className="text-center">
+          <h3 className="titulos-dashboard">ANÁLISIS POR TERRITORIO</h3>
+          <h3 className="titulos-dashboard">TERRITORIO: REGIÓN/CIUDAD</h3>
+        </Row>
+        <Container>
+          <Row className="p-1">
+            <Col className="analisis-boxs m-3 py-3">
+              <div
+                className="d-flex flex-column justify-content-between"
+                style={{ height: "100%" }}
+              >
+                <div className="p-2">
+                  REGIÓN
+                  <Form.Select aria-label="Default select">
+                    <option>Región</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </div>
+                <div className="p-2 ">
+                  CIUDAD
+                  <Form.Select aria-label="Default select">
+                    <option>Ciudad</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </div>
+                <div className="p-2">
+                  {"DIMENSIÓN (ES)"}
+                  <Form.Select aria-label="Default select">
+                    <option>{"Dimensión (es)"}</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                  </Form.Select>
+                </div>
+              </div>
+            </Col>
+            <Col className="analisis-boxs m-3">
+              <div>
+                <RadarChartAnalisis></RadarChartAnalisis>
+              </div>
+            </Col>
+            <Col className="analisis-boxs m-3">
+              <div>Mapa</div>
+            </Col>
+          </Row>
+        </Container>
+      </div>
+    );
+  }
+}
--- src/app/dashboard/components/dashboardFunction/dashboard.js
+++ src/app/dashboard/components/dashboardFunction/dashboard.js
@@ -1,18 +1,15 @@
 import React from "react";
 import { Container, Row , Col } from "react-bootstrap";
 import "./dashboardFunction.css"
+import AnalisisTerritorio from "../analisisTerritorio/analisisTerritorio";
 
 export default class DashboardFunction extends React.Component {
     render() {
         return(
             <div className="superPosition-dashboard">
             <main>
-        <Container className="my-0 background-back-dashboard">
-          <Row className="py-5">
-          <Row className="py-5">
-            
-            </Row>
-          </Row>
+        <Container className="my-0 p-3 background-back-dashboard">
+            <AnalisisTerritorio ></AnalisisTerritorio>
         </Container>
             </main>
             </div>
