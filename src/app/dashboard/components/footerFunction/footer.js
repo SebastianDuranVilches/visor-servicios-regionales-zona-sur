d2864b11e785c4919fa9eb4e98559a6828e27593
SDuran <sduran.vilches@gmail.com>
2023-09-27 00:15:47 -0300

    otras universidades

--- src/app/dashboard/components/footerFunction/footer.js
+++ src/app/dashboard/components/footerFunction/footer.js
@@ -1,33 +1,72 @@
 import React from "react";
-import { Container, Row , Col } from "react-bootstrap";
-import "./footerFuction.css"
-
+import { Container, Row, Col } from "react-bootstrap";
+import "./footerFuction.css";
 
 export default function FooterFunction() {
-    return (
-        <div className="footerFunction pb-2">
-            <div className="super-footer-position pt-4">
-            <Container className="my-1">
-            <Row className="justify-content-between">
-                <Col className="pt-4">
-                <h5 className="text-center">
-                Financia Agencia Nacional de Investigación y Desarrollo, ANID / Fondecyt Regular
-                </h5>
-                <img src="./fond.png" alt="Agencia Nacional de Investigación y Desarrollo" className="mx-auto d-block"/>
-                </Col>
-                <Col className="py-4 ">
-                <h5 className="text-center">
+  return (
+    <div className="footerFunction pb-2">
+      <div className="super-footer-position pt-4">
+        <Container className="my-1">
+          <Row className="justify-content-between">
+            <Col className="pt-4">
+              <h5 className="text-center">
+                Financia Agencia Nacional de Investigación y Desarrollo, ANID /
+                Fondecyt Regular
+              </h5>
+              <img
+                src="./fond.png"
+                alt="Agencia Nacional de Investigación y Desarrollo"
+                className="mx-auto d-block"
+              />
+            </Col>
+            <Col className="py-4 ">
+              <h5 className="text-center">
                 Universidad Patrocinante investigador principal
-                </h5>
-                <img src="./uach.png" alt="UACh" className="mx-auto my-5 d-block" style={{maxWidth : '420px'}}/>
+              </h5>
+              <img
+                src="./uach.png"
+                alt="UACh"
+                className="mx-auto my-5 d-block"
+                style={{ maxWidth: "420px" }}
+              />
+              <h6 className="text-center">
+                Universidades Patrocinantes co investigadores
+              </h6>
+              <Row className="justify-content-between">
+                <Col style={{ maxHeight: "320px", maxWidth: "33%" }}>
+                  <img
+                    src="./logo_PUC.svg"
+                    alt="PUC"
+                    className="mx-auto my-4 d-block"
+                    style={{ maxWidth: "39%" }}
+                  />
                 </Col>
-            </Row>            
-            <Row className="pt-5">
-            <p className="text-center">Por Sebastián Andres Durán Vilches</p>
-            </Row>
-            </Container>
-            </div>
-        </div>
-    );
+                <Col style={{ maxHeight: "320px", maxWidth: "33%" }}>
+                  <img
+                    src="./logo_UAH_2.svg"
+                    alt="UAH"
+                    className="mx-auto my-5 d-block"
+                    style={{ maxWidth: "100%" }}
+                  />
+                </Col>
+                <Col style={{ maxHeight: "320px", maxWidth: "33%" }}>
+                  <img
+                    src="./logo_UCT.svg"
+                    alt="UCT"
+                    className="mx-auto my-5 d-block"
+                    style={{ maxWidth: "100%" }}
+                  />
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+          <Row className="pt-5">
+            <p className="text-center">
+              Desarrollado por Sebastián Andres Durán Vilches - Septiembre 2023
+            </p>
+          </Row>
+        </Container>
+      </div>
+    </div>
+  );
 }
-
